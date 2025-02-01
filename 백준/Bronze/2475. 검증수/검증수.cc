122b6799453b5ecd885e64b341ff86a5d93720e1
Kim ye been <126430033+whattheyeb@users.noreply.github.com>
2025-02-01 19:46:21 +0900

    [Bronze V] Title: 검증수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num, sum = 0;
+
+    for (int i = 0; i < 5; i++) {
+        cin >> num;
+        sum += num * num;
+    }
+
+    cout << sum % 10 << endl;
+
+    return 0;
+}

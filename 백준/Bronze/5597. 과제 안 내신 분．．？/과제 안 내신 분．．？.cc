e13aefb785f7ce08482fa197b7fe027a51cb815f
kind mouse <126430033+whattheyeb@users.noreply.github.com>
2025-09-07 13:57:42 +0300

    [Bronze III] Title: 과제 안 내신 분..?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.cc
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.cc
@@ -6,30 +6,25 @@ using namespace std;
 int main(){
     int kind_student_cnt = 28;
     int stu_num;
-    vector<int> bad_stu(3);
-    vector<int> stu_check(30);
+    vector<int> bad_stu;
+
+    // 0/1 만 쓰고있기때문에 불리언 타입을 쓰는 것이 더 좋다
+    vector<bool> stu_check(30);
 
     for(int i = 0; i < kind_student_cnt; i++){
         cin >> stu_num;
         stu_check[stu_num - 1] = 1;
     }
     
-    int cnt = 0;
+    // 처음 vector 크기를 정하지않은채로 선언하고, push_back를 사용한다. 
     for(int i = 0; i < stu_check.size(); i++){
-        if(stu_check[i] == 0){ 
-            bad_stu[cnt] = i + 1;
-            cnt++;
+        if(stu_check[i] == 0){
+            bad_stu.push_back(i+1);
         }
     }
 
-    if(bad_stu[1] > bad_stu[0]){
-        for(int i = 0; i < 2; i++){
+    for(int i = 0; i < 2; i++){
             cout << bad_stu[i] << "\n";
-        }
-    }else{
-        for(int j = 1; j >= 0; j--){
-            cout << bad_stu[j] << "\n";
-        }
     }
     
 }

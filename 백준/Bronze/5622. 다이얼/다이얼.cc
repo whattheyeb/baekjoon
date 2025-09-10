79fc14b92c69b7c7645d4c52d170f1cdf725d92a
kind mouse <126430033+whattheyeb@users.noreply.github.com>
2025-09-10 22:11:56 +0300

    [Bronze II] Title: 다이얼, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/5622. 다이얼/다이얼.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    string input;
+
+    cin >> input;
+
+    int sum = 0;
+
+    for(int i = 0; i < input.size(); i++){
+        if(input[i] == 'A' || input[i] == 'B' || input[i] == 'C'){
+            sum += 3;
+        }else if(input[i] == 'D' || input[i] == 'E' || input[i] == 'F'){
+            sum += 4;
+        }else if(input[i] == 'G' || input[i] == 'H' || input[i] == 'I'){
+            sum += 5;    
+        }else if(input[i] == 'J' || input[i] == 'K' || input[i] == 'L'){
+            sum += 6;
+        }else if(input[i] == 'M' || input[i] == 'N' || input[i] == 'O'){
+            sum += 7;
+        }else if(input[i] == 'P' || input[i] == 'Q' || input[i] == 'R' || input[i] == 'S'){
+            sum += 8;
+        }else if(input[i] == 'T' || input[i] == 'U' || input[i] == 'V'){
+            sum += 9;
+        }else if(input[i] == 'W' || input[i] == 'X' || input[i] == 'Y' || input[i] == 'Z'){
+            sum += 10;
+        }
+    }
+
+    cout << sum << "\n";
+}

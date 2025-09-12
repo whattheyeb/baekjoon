16ebbd65d6828eef4fa38dd1cce5507d0ed93bfc
kind mouse <126430033+whattheyeb@users.noreply.github.com>
2025-09-12 15:39:43 +0300

    [Bronze V] Title: 이상한 기호, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15964. 이상한 기호/이상한 기호.cc
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.cc
@@ -5,6 +5,6 @@ using namespace std;
 int main(){
     int a, b;
     cin >> a >> b;
-    long int output = (a + b) * (a - b);
-    cout << output << endl;
+    long long int output = 1LL * (a + b) * (a - b);
+    cout << output << "\n";
 }

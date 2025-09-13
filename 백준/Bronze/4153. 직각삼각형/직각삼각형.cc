e8a6dfd51f5ec549a1c9d00c62faca6a94223b65
kind mouse <126430033+whattheyeb@users.noreply.github.com>
2025-09-13 14:55:26 +0300

    [Bronze III] Title: 직각삼각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/4153. 직각삼각형/직각삼각형.cc
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.cc
@@ -1,9 +1,11 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-void isRight(int max, int a1, int b1){
-    if((max * max) == (a1 * a1 + b1 * b1)){
+void isRight(int hyp, int a1, int b1){
+    if((hyp * hyp) == (a1 * a1 + b1 * b1)){
         cout << "right" << "\n";
     }else{
         cout << "wrong" << "\n";
@@ -12,26 +14,15 @@ void isRight(int max, int a1, int b1){
 
 int main(){
     int len1, len2, len3;
-    int max;
-    do{
-        cin >> len1 >> len2 >> len3;
-        if(len1 == 0 && len2 == 0 && len3 == 0){
+    // max 는 표준 함수 std::max 와 의미가 겹친다.
+    int hyp;
+    int a[3];
+
+    while(cin >> a[0] >> a[1] >> a[2]){
+        if(a[0] == 0 && a[1] == 0 && a[2] == 0){
             break;
         }
-        max = len1;
-        if(len1 < len2){
-            max = len2;
-            if(len2 < len3) max = len3;
-        }else if(len1 < len3){
-            max = len3;
-            if(len3 < len2) max = len2;
-        }
-        if(max == len1){
-            isRight(max, len2, len3);
-        }else if(max == len2){
-            isRight(max, len1, len3);
-        }else if(max == len3){
-            isRight(max, len1, len2);
-        }
-    }while(len1 != 0 || len2 != 0 || len3 != 0);
+        sort(begin(a), end(a));
+        isRight(a[2], a[1], a[0]);
+    }
 }

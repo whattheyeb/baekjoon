1c0de684b62c1341841e39dff12449e51c4c5abd
kind mouse <126430033+whattheyeb@users.noreply.github.com>
2025-10-02 15:54:59 +0300

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    long long int n;
+    cin >> n;
+    long long int result = n * (n - 1) / 2;
+    cout << result << '\n' << 2 << '\n';
+
+}

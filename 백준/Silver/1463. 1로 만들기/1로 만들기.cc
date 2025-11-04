f6566c2700f2dddbd2ee6eef19615545058a06f2
kind mouse <126430033+whattheyeb@users.noreply.github.com>
2025-11-04 17:49:53 +0200

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    int cnt = 0;
+    vector<int> dp(n + 1, 0);
+    dp[1] = 0;
+    dp[2] = 1;
+    dp[3] = 1;
+    for(int i = 4; i <= n; i++){
+        dp[i] = dp[i - 1] + 1;
+        if(i % 3 == 0){
+            dp[i] = min(dp[i], dp[i / 3] + 1);
+        }
+        if(i % 2 == 0){
+            dp[i] = min(dp[i], dp[i / 2] + 1);
+        }
+    }
+    cout << dp[n] << '\n';
+}

4d319d193bf569b3a5cb332f67c95f9e2657f67a
kind mouse <126430033+whattheyeb@users.noreply.github.com>
2025-11-30 20:47:52 +0200

    [Silver III] Title: N과 M (4), Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int n, m;
+int arr[8];
+int result[8];
+int visited[8];
+
+// 중복순열
+void permutation(int depth){
+    if(m == depth){
+        for(int i = 0; i < m; i++){
+            cout << result[i] << ' '; 
+        }
+        cout << '\n';
+        return;
+    }
+    for(int i = 1; i <= n; i++){
+        if((depth == 0) || (result[depth - 1] <= i)){
+            result[depth] = i;
+            permutation(depth + 1);
+        }
+        
+        
+    }
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cout.tie(nullptr);
+    cin >> n >> m;
+    permutation(0);
+    return 0;
+
+}
